212c7cc1f4afd767052c28f0be15e31053924ba8
Callan <30566513+BaTmAnOmOrE@users.noreply.github.com>
2022-06-23 16:49:44 +1200

    added repo and retrieved API key from website

--- client/apiClient.js
+++ client/apiClient.js
@@ -6,4 +6,7 @@ const serverURL = 'http://localhost:3000/api/v1'
 export function getWelcome() {
   return request.get(`${serverURL}/welcome`).then((response) => response.body)
 }
+export function getNews(){
+  return request.get('https://newsapi.org/v2/everything?q=tesla&from=2022-05-23&sortBy=publishedAt&apiKey=2bf36f72f3df4b84a2bbf66178d350b2').then(response =>response.body)
+}
 // ***   ***   ***
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
-
-import { getWelcome } from '../apiClient'
+import { get } from 'superagent'
+import { getWelcome, getNews } from '../apiClient'
 
 function App() {
   const [welcomeStatement, setWelcomeStatement] = useState('')
-
+  const [news, setNews] = useState(null)
   useEffect(() => {
     getWelcome()
       .then((res) => {
@@ -14,8 +14,31 @@ function App() {
         console.error(err.message)
       })
   })
+  getNews()
+    .then((res) => {
+      setNews(res)
+      console.log(res)
+      return null
+    })
+    .catch((err) => {
+      console.error(err.message)
+    })
 
-  return <h1>{welcomeStatement}</h1>
+  return (
+    <>
+      <h1>{welcomeStatement}</h1>
+      <div>
+        {pokemon && (
+          <div>
+            <h2>{articles.title}</h2>
+            <p>Id: {pokemon.id}</p>
+            <p>Base experience: {pokemon.base_experience}</p>
+            <p>Height: {pokemon.height}</p>
+          </div>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default App
